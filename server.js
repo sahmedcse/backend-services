55adc04f1c00b25f6bb3a10d6dd24bda32029239
sadatahmed <s78ahmed@uwaterloo.ca>
2016-07-15 11:35:01 -0400

    added send grid API to send emails. Currently sending emails properly.

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var email = require('sendgrid').mail;
 
+app.use(bodyParser.json());
+
+var server = app.listen(process.env.PORT || 3000, function() {
+  var port = server.address().port;
+  console.log("Server running on port", port);
+});
+
+app.post("/email", function(request, response) {
+  var emailJson = request.body;
+  console.log("JsonObj ", emailJson);
+
+  sendEmail(emailJson);
+
+  response.status(200).end();
+
+});
+
+function sendEmail(emailJson) {
+  var emailFrom = new email.Email(emailJson.emailFrom);
+  var emailTo = new email.Email(emailJson.emailTo);
+  var subject = emailJson.subject;
+  var content = new email.Content("text/plain", emailJson.emailContent);
+  var mail = new email.Mail(emailFrom, subject, emailTo, content);
+
+  var sg = require('sendgrid').SendGrid("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");
+
+  var requestBody = mail.toJSON();
+  var request = sg.emptyRequest();
+  request.method = 'POST';
+  request.path = '/v3/mail/send';
+  request.body = requestBody;
+  sg.API(request, function (response) {
+    console.log(response.statusCode);
+  });
+};
